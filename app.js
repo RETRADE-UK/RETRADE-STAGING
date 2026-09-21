e941730ddd99373ff7fc40e5cfb70f2d81f23999
RETRADE-UK <general007store@gmail.com>
2026-09-21 21:35:10 +0100

    Rotate staging to production v1.5.37 under gesture G1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
  */
 (function(){
   'use strict';
-  var v='20260921-v1536-staging-g1';
+  var v='20260921-v1537-staging-g1';
   window.__rtBuildId=v;
   var motionReady=false;
   var motionFallbackTimer=0;
